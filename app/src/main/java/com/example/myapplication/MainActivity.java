9071f7ee2855fa7ca680853970c2e0ccbd7bfae6
unknown <vcsonawane99@gmail.com>
2020-09-24 14:03:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeFragment(View view){
+
+        Fragment fragment;
+
+        if (view == findViewById(R.id.button1)){
+            fragment = new FragmentOne();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place,fragment);
+            ft.commit();
+        }
+
+        if (view == findViewById(R.id.button2)){
+            fragment = new FragmentTwo();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place,fragment);
+            ft.commit();
+        }
+    }
+}
